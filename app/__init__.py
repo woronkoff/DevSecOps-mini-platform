51eabf8904f65af819b81228565771bdfb5d6679
Rafael Woronkoff Montanha <rflmontanha@gmail.com>
2026-06-18 20:42:40 +0200

    Day 8: add secret handling in CI

--- app/__init__.py
+++ app/__init__.py
@@ -10,9 +10,9 @@ def create_app():
     app = Flask(__name__)
 
     # Load configuration from environment
-    app.config["APP_NAME"] = os.getenv("APP_NAME")
-    app.config["APP_VERSION"] = os.getenv("APP_VERSION")
-    app.config["APP_ENV"] = os.getenv("APP_ENV")
+    app.config["APP_NAME"] = os.getenv("APP_NAME", "DevSecOps Mini Platform")
+    app.config["APP_VERSION"] = os.getenv("APP_VERSION", "0.1.0")
+    app.config["APP_ENV"] = os.getenv("APP_ENV", "development")
 
     from app.routes import main
     app.register_blueprint(main)
